f6e0374a8092432e31c8ec43d6a4254fd3c12fe8
Exntpf <z5206482@ad.unsw.edu.au>
2023-01-04 00:24:18 +1100

    added extra xml functionality and refactored

--- xml_practice/src/main.rs
+++ xml_practice/src/main.rs
@@ -1,15 +1,11 @@
-use std::cmp::Ordering;
-use std::env::temp_dir;
-use std::error::Error;
-use std::fs::File;
-use std::io::{BufReader, BufRead, ErrorKind, Error as IoError, Bytes};
+use std::io::{BufRead, ErrorKind};
 use std::ops::Deref;
 use std::path::Path;
 // use std::collections::HashMap;
 use std::str;
 
 use quick_xml::Error as XmlError;
-use quick_xml::events::{Event, BytesStart, BytesText};
+use quick_xml::events::{Event, BytesStart};
 // use quick_xml::events::{attributes};
 use quick_xml::reader::Reader;
 
@@ -39,108 +35,6 @@ fn contains_att_value(event: &BytesStart, key: &str, value: &str) -> Result<bool
     }
     Ok(false)
 }
-
-/// Input: file name, name of the tag, an attribute key-value pair
-/// returns: byte array with the content that appears after the '>' 
-/// of the tag found, trimmed of leading and trailing whitespace, each
-/// tag ending in a newline '\n' character
-pub fn get_first_content<'a,  P: AsRef<Path>>(file_path: P, name: &'a str, att_key: &'a str, att_value: &'a str) -> Result<Vec<u8>, XmlError>{
-    let mut reader = Reader::from_file(file_path)?;
-    reader.trim_text(true);
-    let mut buf = Vec::new();
-    
-    // locate the tag in question
-    let event = loop{
-        let event = reader.read_event_into(&mut buf)?;
-        match event {
-            Event::Start(ref e) => {
-                if event_has_name(e, name) && 
-                contains_att_value(e, att_key, att_value)
-                .expect("resource search: unexpected error occurred") {
-                    break event.to_owned();
-                }                
-            },
-            Event::Empty(ref e) => {
-                if event_has_name(&e, name) && 
-                contains_att_value(&e, att_key, att_value)
-                .expect("resource search: unexpected error occurred") {
-                    let mut output = Vec::from( event
-                        .into_owned()
-                        .deref()
-                        .to_owned()
-                        );
-                    output.push(b'\n');
-                    return Ok(output);
-                }   
-            }
-            Event::Eof => { return Err(XmlError::TextNotFound) }
-            _ => {},
-        }
-        buf.clear();
-    };
-
-    let mut output: Vec<u8> = Vec::new();
-    let mut buf2 = Vec::new();
-
-    // we know that event is now Event::Start, so the `else` case
-    // should never occur.
-    if let Event::Start(event_content) = event {
-        // add all other tags into output vector till Event::End
-        loop{
-            let event = reader.read_event_into(&mut buf2)?;
-            match event {
-
-                Event::End(e) => {
-                    if e.name().cmp(&event_content.name()).is_eq(){
-                        break;
-                    } else {
-                        output.append(
-                            &mut Event::End(e)
-                            .into_owned()
-                            .deref()
-                            .to_owned()
-                            );
-                        output.push(b'\n');
-                    }
-                },
-                Event::Eof => return Err(XmlError::UnexpectedEof(
-                    "reach end of file without finding closing tag".to_owned()
-                )),
-                _ => {
-                    output.append(&mut event
-                                    .into_owned()
-                                    .deref()
-                                    .to_owned()
-                                    );
-                    output.push(b'\n');
-                },
-            }
-            buf2.clear();
-        };
-    } else {
-        panic!("mysteriously, event was not Event::Start");
-    }
-    Ok(output)
-}
-
-/* 
- * fn seek_till<'a, R: BufRead, T: AsRef<Path>>(name: &'a str, file_path: &'a T) -> Result<(Reader<BufReader<File>>, Vec<u8>, Event<'a>), Error>;
- * finds first occurence of of either Start or Empty tag with name="name"
- * returns Reader at that position, else Err - which would be a useful function
- * and after trying to figure this out for way too long, I am officially 
- * out of patience and am not not bothering with lifetimes, event returning,
- * function definitions or any of that stuff. I am writing this the dumb
- * block of code spaghetti way and that's that. If you can figure this
- * out please do so and I will be more than happy to see how someone
- * not a newbie solves this problem
- */ 
-
-// Other exercises in coupling include: 
-    // after reading in the "resource" event, read in  the <doc> and </doc>
-    // by calling .read_event_into 2 times to get to the method tag
-    // we can make this assumption because the sep_wadl.xml
-    // file we are using and coupling to this code has only
-    // this format. However, this does very tightly couple the two
 
 /// wadl.rs
 #[derive(Debug)]
@@ -214,23 +108,177 @@ fn get_mode(path_arg: &str, method_arg: &str) -> Option<Mode>{
     }
 }
 
+/* 
+ * fn seek_till<'a, R: BufRead, T: AsRef<Path>>(name: &'a str, file_path: &'a T) -> Result<(Reader<BufReader<File>>, Vec<u8>, Event<'a>), Error>;
+ * finds first occurence of of either Start or Empty tag with name="name"
+ * returns Reader at that position, else Err - which would be a useful function
+ * and after trying to figure this out for way too long, I am officially 
+ * out of patience and am not not bothering with lifetimes, event returning,
+ * function definitions or any of that stuff. I am writing this the dumb
+ * block of code spaghetti way and that's that. If you can figure this
+ * out please do so and I will be more than happy to see how someone
+ * not a newbie solves this problem
+ */ 
+fn seek_till<'a, R: BufRead>(reader: &mut Reader<R>, name: &str, att_key: Option<&str>, att_value: Option<&str>) -> Result<Event<'static>, XmlError>{
+    let mut buf = Vec::new();
+    
+    // locate the tag in question
+    loop{
+        let event = reader.read_event_into(&mut buf)?;
+        match event {
+            Event::Start(ref e) => {
+                if !event_has_name(e, name){
+                    continue;
+                }
+                if let (Some(att_key), Some(att_value)) = (att_key, att_value){
+                    if contains_att_value(&e, att_key, att_value)?{
+                        return Ok(event.into_owned());
+                    }
+                } else {
+                    return Ok(event.into_owned());
+                }
+            },
+            Event::Empty(ref e) => {
+                if !event_has_name(e, name){
+                    continue;
+                }
+                if let (Some(att_key), Some(att_value)) = (att_key, att_value){
+                    if contains_att_value(&e, att_key, att_value)?{
+                        return Ok(event.into_owned());
+                    }
+                } else {
+                    return Ok(event.into_owned());
+                }
+            },
+            Event::Eof => { return Err(XmlError::TextNotFound) }
+            _ => {},
+        }
+        buf.clear();
+    };
+}
+
+// Other potential exercises in coupling include: 
+    // after reading in the "resource" event, read in  the <doc> and </doc>
+    // by calling .read_event_into 2 times to get to the method tag
+    // we can make this assumption because the sep_wadl.xml
+    // file we are using and coupling to this code has only
+    // this format. However, this does very tightly couple the two
+
+/// Input: file name, name of the tag, an attribute key-value pair
+/// returns: byte array with the content that appears after the '>' 
+/// of the tag found, trimmed of leading and trailing whitespace, each
+/// tag ending in a newline '\n' character
+pub fn get_first_content<P: AsRef<Path>>(file_path: P, name: &str, att_key: Option<&str>, att_value: Option<&str>) -> Result<Vec<u8>, XmlError>{
+    let mut reader = Reader::from_file(file_path)?;
+    reader.trim_text(true);
+    let mut buf = Vec::new();
+    let mut output: Vec<u8> = Vec::new();
+
+    let found_event = seek_till(&mut reader, name, att_key, att_value)?;
+    // found_event is either Empty or Start. If Empty, we return
+    match found_event {
+        Event::Empty(e) =>{
+            let mut output = Vec::from( e
+                .deref()
+                .to_owned()
+                );
+            output.push(b'\n');
+            return Ok(output);
+        },
+        Event::Start(found_event_content) =>{
+            loop{
+                let event = reader.read_event_into(&mut buf)?;
+                match event {
+                    Event::End(e) => {
+                        if e.name().cmp(&found_event_content.name()).is_eq(){
+                            break;
+                        } else {
+                            output.append(
+                                &mut Event::End(e)
+                                .into_owned()
+                                .deref()
+                                .to_owned()
+                                );
+                            output.push(b'\n');
+                        }
+                    },
+                    Event::Eof => return Err(XmlError::UnexpectedEof(
+                        "reach end of file without finding closing tag".to_owned()
+                    )),
+                    _ => {
+                        output.append(&mut event
+                                        .into_owned()
+                                        .deref()
+                                        .to_owned()
+                                        );
+                        output.push(b'\n');
+                    },
+                }
+                buf.clear();
+            };
+        },
+        _ => { return Err(XmlError::TextNotFound) }    
+    }
+    Ok(output)
+}
+
+/// retrieves first instance of a Start or Empty tag with "name" and Some("att_key")=Some("att_value")
+/// if att_key and/or att_value are None, ignores tag attributes.
+/// returns the found tag
+fn get_tag_bytes<P: AsRef<Path>>(file_path: P, name: &str, att_key: Option<&str>, att_value: Option<&str>) -> Result<Vec<u8>, XmlError>{
+    let mut reader = Reader::from_file(file_path)?;
+    reader.trim_text(true);
+    let found_tag = seek_till(&mut reader, name, att_key, att_value)?;
+    println!("get_tag_bytes output: {found_tag:?}");
+    return Ok(found_tag.deref().to_vec());
+}
+
+
+
+const WADL_PATH: &str = "sep_wadl.xml";
+
+pub fn validate_method(path: &str, method: &str) -> Option<Mode>{
+    let resource_tag = get_tag_bytes(
+                        WADL_PATH, 
+                        "resource",
+                        Some("wx:samplePath"),
+                        Some(path));
+    match resource_tag {
+        Ok(bytes) =>{
+            let resource_str = str::from_utf8(&bytes).unwrap();
+            println!("resource_str: {resource_str}");
+            if let Some(id) = resource_str
+            .split_whitespace()
+            .filter(|a| a.starts_with("id"))
+            .next(){
+                println!("{id}");
+                Some(Mode::Mandatory)
+            } else {
+                panic!("sep_wadl contained incorrect whitespace: {resource_str}");
+            }
+        },
+        Err(e) => {
+            match e {
+                quick_xml::Error::Io(_) => panic!("sep_wadl.xml file not found"),
+                quick_xml::Error::NonDecodable(_) => panic!("sep_wadl.xml file decoding error"),
+                quick_xml::Error::UnexpectedEof(_) => None,
+                quick_xml::Error::InvalidAttr(_) => None,
+                XmlError::TextNotFound => return None,
+                _ => panic!("Unexpected error occurred"),
+            }
+        },
+    }
+}
 
 fn main() {
-    let path_arg = "/sdev";
+    let path_arg = "/dcap";
     let method_arg = "GET";
     let path = "sep_wadl.xml";
     let tag_name = "method";
     let att_key = "id";
-    let att_value = "GETDeviceCapability";
-    let found_mode = get_mode(path_arg, method_arg);
-    println!("Mode found for path {path_arg} and method {method_arg}: {:?}", found_mode);
-    if let Ok(response) = get_first_content(path, tag_name, att_key, att_value){
+    let att_value = "POSTDeviceCapability";
 
-        println!("content of {tag_name} with {att_key}:{att_value} :");
-        println!("{}", str::from_utf8(&response).unwrap());
-    }else {
-        println!("something went majorly wrong");
-    }
+    println!("{:?}", validate_method(path_arg, method_arg));
 }
 
 
